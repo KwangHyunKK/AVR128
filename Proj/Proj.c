328473cbb545aa17b478617dd5f58c56bf944c64
Kwang Hyun Kim <panghyun35@naver.com>
2023-06-15 14:54:40 +0900

    [#1] Line 46 : int char => char Line 109 : TCRR2 => TCCR2 Line 136 : i= 추가 Line 154 : j = 추가

--- Proj/Proj.c
+++ Proj/Proj.c
@@ -8,13 +8,13 @@
 #define DISTANCE_PER_FLOOR 1000 // 실제 이동해야 하는 거리
 #define QUEUE_SIZE 20 // queue size
 #define MAX_HEIGHT 10 // 엘리베이터의 높이
-static unsigned int queue[QUEUE_SIZE]; //  
+static unsigned int queue[QUEUE_SIZE]; //
 static unsigned int left, right; // 큐의 좌/우측에 넣을 값들
 static unsigned int velocity, acceleration=10; // 속도 / 가속도
 static unsigned int current_floor, diff_floor, object_floor; // 목표 층과 현재 층
 static unsigned char is_open, is_move, is_up, has_open; // 움직일 수 있는지, 문을 열 수 있는지 check
 static unsigned long times1, times2; // 시간 check
-static unsigned int duty = 0; // duty 값 
+static unsigned int duty = 0; // duty 값
 
 // 함수 정의
 
@@ -25,25 +25,25 @@ char check_duplication(unsigned int value);
 
 
 const unsigned char seven_seg_digits_decode_gfedcba[75]= {
-/*  0     1     2     3     4     5     6     7     8     9     :     ;     */
-    0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x00, 0x00, 
-/*  <     =     >     ?     @     A     B     C     D     E     F     G     */
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71, 0x3D, 
-/*  H     I     J     K     L     M     N     O     P     Q     R     S     */
-    0x76, 0x30, 0x1E, 0x75, 0x38, 0x55, 0x54, 0x5C, 0x73, 0x67, 0x50, 0x6D, 
-/*  T     U     V     W     X     Y     Z     [     \     ]     ^     _     */
-    0x78, 0x3E, 0x1C, 0x1D, 0x64, 0x6E, 0x5B, 0x00, 0x00, 0x00, 0x00, 0x00, 
-/*  `     a     b     c     d     e     f     g     h     i     j     k     */
-    0x00, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71, 0x3D, 0x76, 0x30, 0x1E, 0x75, 
-/*  l     m     n     o     p     q     r     s     t     u     v     w     */
-    0x38, 0x55, 0x54, 0x5C, 0x73, 0x67, 0x50, 0x6D, 0x78, 0x3E, 0x1C, 0x1D, 
-/*  x     y     z     */
-    0x64, 0x6E, 0x5B
-}; // 7-segment ascii 코드와 대응하는 배열 
-unsigned char fnd_sel[4] = {0x01, 0x02, 0x04, 0x08}; // 7-segment selection 
+	/*  0     1     2     3     4     5     6     7     8     9     :     ;     */
+	0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0x00, 0x00,
+	/*  <     =     >     ?     @     A     B     C     D     E     F     G     */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71, 0x3D,
+	/*  H     I     J     K     L     M     N     O     P     Q     R     S     */
+	0x76, 0x30, 0x1E, 0x75, 0x38, 0x55, 0x54, 0x5C, 0x73, 0x67, 0x50, 0x6D,
+	/*  T     U     V     W     X     Y     Z     [     \     ]     ^     _     */
+	0x78, 0x3E, 0x1C, 0x1D, 0x64, 0x6E, 0x5B, 0x00, 0x00, 0x00, 0x00, 0x00,
+	/*  `     a     b     c     d     e     f     g     h     i     j     k     */
+	0x00, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71, 0x3D, 0x76, 0x30, 0x1E, 0x75,
+	/*  l     m     n     o     p     q     r     s     t     u     v     w     */
+	0x38, 0x55, 0x54, 0x5C, 0x73, 0x67, 0x50, 0x6D, 0x78, 0x3E, 0x1C, 0x1D,
+	/*  x     y     z     */
+	0x64, 0x6E, 0x5B
+}; // 7-segment ascii 코드와 대응하는 배열
+unsigned char fnd_sel[4] = {0x01, 0x02, 0x04, 0x08}; // 7-segment selection
 unsigned char fnd[4]; //
-unsigned char digits2[2]; // 값을 받아서 저장할 값  
-unsigned int char digits_idx=0;
+unsigned char digits2[2]; // 값을 받아서 저장할 값
+unsigned char digits_idx=0;
 
 // 1. LED setting
 void init_LED()
@@ -88,72 +88,72 @@ unsigned char gettingnumber(); // 1 char 수신
 
 void init_bluetooth()
 {
-    UBRR0H = 0;
-    UBRR0L = 0;
-    UCSR0B = 0x18; // 0b 0001 1000 : USART Empty interrupt, RXENn 
-    UCSR0C = 0x06; // 0b 0000 0110 : 8bit 
+	UBRR0H = 0;
+	UBRR0L = 0;
+	UCSR0B = 0x18; // 0b 0001 1000 : USART Empty interrupt, RXENn
+	UCSR0C = 0x06; // 0b 0000 0110 : 8bit
 }
 
 // 5. ADC setting
 void init_ADC()
 {
-    ADMUX = 0x00;
-    ADCSRA = 0x87; // 0b 1000 0111 : ADEN, 128 division factor
+	ADMUX = 0x00;
+	ADCSRA = 0x87; // 0b 1000 0111 : ADEN, 128 division factor
 }
 
 // 6. PWM setting => Fast PWM 사용
 void init_PWM()
 {
-    TIMSK = 0x40; // TOV 인터럽트에서 사용 : CLOCK
-    TCCR2 = (1 << WGM01) | (1 << WGM00) | (2 << COM00); // Fast PWM 모드, 비교 일치 떄 동작모드
-    TCRR2 |= (3 << CS00); // 분주비 8 설정 (2) // 3 => 64로 설정
-    OCR2 = 0;
-    // OCR2 = duty;
+	TIMSK = 0x40; // TOV 인터럽트에서 사용 : CLOCK
+	TCCR2 = (1 << WGM01) | (1 << WGM00) | (2 << COM00); // Fast PWM 모드, 비교 일치 떄 동작모드
+	TCCR2 |= (3 << CS00); // 분주비 8 설정 (2) // 3 => 64로 설정
+	OCR2 = 0;
+	// OCR2 = duty;
 }
 
-// 7. MOTOR setting => 
+// 7. MOTOR setting =>
 void init_MOTOR()
 {
-    DDRB = 0x0A;
+	DDRB = 0x0A;
 }
 
 void init()
 {
-    init_7seg();
-    init_ADC();
-    init_bluetooth();
-    init_button();
-    init_LED();
-    init_PWM(); 
+	init_7seg();
+	init_ADC();
+	init_bluetooth();
+	init_button();
+	init_LED();
+	init_PWM();
 }
 
 // 사용 함수 정의
 char check_duplication(unsigned int value)
 {
-    if(left == right)return 0;
-    else
-    {
-        for(int i=left;i!=right;(i + 1)%QUEUE_SIZE)
-        {
-            if(value == queue[i])return i;
-        }
-    }
-    return -1;
+	if(left == right)return 0;
+	else
+	{
+		for(int i=left;i!=right;i=(i + 1)%QUEUE_SIZE)
+		{
+			if(value == queue[i])return i;
+		}
+	}
+	return -1;
 }
 
 // 변경
 void cancel_floor(int idx)
 {
-    if(idx<0)return;
-    // 바로 다음인 경우에는 다음의 값으로 선택될 수 있기 때문에 바로 취소할 수 없다.
-    // 거리가 2 이상인 경우에 취소가 가능
-    if(idx != (left + 1)%QUEUE_SIZE)
-    {
-        if(idx == right)--right;
-        else{
-            for(int j=idx;j!=right;(j+1)%QUEUE_SIZE)queue[j] = queue[(j+1)%QUEUE_SIZE];
-        }
-    }
+	if(idx<0)return;
+	// 바로 다음인 경우에는 다음의 값으로 선택될 수 있기 때문에 바로 취소할 수 없다.
+	// 거리가 2 이상인 경우에 취소가 가능
+	if(idx != (left + 1)%QUEUE_SIZE)
+	{
+		if(idx == right)--right;
+		else{
+			for(int j=idx;j!=right;j=(j+1)%QUEUE_SIZE)queue[j] = queue[(j+1)%QUEUE_SIZE];
+		}
+	}
 }
 
 
@@ -201,7 +201,7 @@ ISR(TIMER2_COMP_vect)
 		// 층 변화에 따른 변화 check
 		if(is_up) // current_floor < object_floor // 정회전
 		{
-            PORTB = 0x80;
+			PORTB = 0x80;
 			// 1층 이동
 			if(object_floor - current_floor > diff_floor/2)
 			{
@@ -230,7 +230,7 @@ ISR(TIMER2_COMP_vect)
 			}
 		}else // object_floor > current_floor // 역회전
 		{
-            PORTB = 0x40;
+			PORTB = 0x40;
 			// 1층 이동
 			if(current_floor - object_floor > diff_floor/2)
 			{
@@ -258,7 +258,7 @@ ISR(TIMER2_COMP_vect)
 				}
 			}
 		}
-        OCR2 = velocity;
+		OCR2 = velocity;
 	}
 	else // 멈춰있는 상태
 	{
@@ -349,29 +349,29 @@ ISR(USART0_RX_vect)
 		{
 			if((right + 1)%QUEUE_SIZE != left)
 			{
-                int c = check_duplication(value);
-                if(c>=0)cancel_floor(c);
-                else
-                {
-                    unsigned int i = (right + 1)%QUEUE_SIZE;
-                    queue[i] = value;
-                    set_7seg_char(3, 'N');
-                    set_7seg_char(2, '=');
-                    set_7seg_num(1, value/10);
-                    set_7seg_num(0, value%10);
-                }
+				int c = check_duplication(value);
+				if(c>=0)cancel_floor(c);
+				else
+				{
+					unsigned int i = (right + 1)%QUEUE_SIZE;
+					queue[i] = value;
+					set_7seg_char(3, 'N');
+					set_7seg_char(2, '=');
+					set_7seg_num(1, value/10);
+					set_7seg_num(0, value%10);
+				}
 			}
 			else // 배열이 꽉 차서 값이 못 들어간다
 			{
-                int c = check_duplication(value);
-                if(c>=0)cancel_floor(c);
-                else
-                {
-                    set_7seg_char(3, 'F');
-                    set_7seg_char(2, 'U');
-                    set_7seg_char(1, 'L');
-                    set_7seg_char(0, 'L');
-                }
+				int c = check_duplication(value);
+				if(c>=0)cancel_floor(c);
+				else
+				{
+					set_7seg_char(3, 'F');
+					set_7seg_char(2, 'U');
+					set_7seg_char(1, 'L');
+					set_7seg_char(0, 'L');
+				}
 			}
 		}
 	}
@@ -399,15 +399,15 @@ ISR(USART0_RX_vect)
 
 int main()
 {
-    sei(); // 전역 인터럽트 허용
-    init(); // init_ 을 모은 함수
-    while(1)
-    {
-        // 조도 센서를 통한 LED 제어
-        set_ADCLED();
-        OCR2 = duty;
-    }
-    return 0;
+	sei(); // 전역 인터럽트 허용
+	init(); // init_ 을 모은 함수
+	while(1)
+	{
+		// 조도 센서를 통한 LED 제어
+		set_ADCLED();
+		OCR2 = duty;
+	}
+	return 0;
 }
 
 void msec_delay(int msec)
@@ -449,14 +449,14 @@ unsigned short read_adc()
 
 void set_ADCLED()
 {
-    unsigned short value = read_adc();
-    if(value > 1000)PORTA = 0x00;
-    else if(value > 875)PORTA = 0x01;
-    else if(value > 750)PORTA = 0x03;
-    else if(value > 625)PORTA = 0x07;
-    else if(value > 500)PORTA = 0x0F;
-    else if(value > 375)PORTA = 0x1F;
-    else if(value > 250)PORTA = 0x3F;
-    else if(value > 125)PORTA = 0x7F;
-    else PORTA = 0xFF;
+	unsigned short value = read_adc();
+	if(value > 1000)PORTA = 0x00;
+	else if(value > 875)PORTA = 0x01;
+	else if(value > 750)PORTA = 0x03;
+	else if(value > 625)PORTA = 0x07;
+	else if(value > 500)PORTA = 0x0F;
+	else if(value > 375)PORTA = 0x1F;
+	else if(value > 250)PORTA = 0x3F;
+	else if(value > 125)PORTA = 0x7F;
+	else PORTA = 0xFF;
 }
